0a389a40aa1cff565e88f0e134b2a462b8c0fa59
Mohamed Amine LOUATI <med-amine.louati@soprahr.com>
2023-10-25 15:02:43 +0100

    service commit

--- src/main/java/atos/user/register/service/UserService.java
+++ src/main/java/atos/user/register/service/UserService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import atos.user.register.model.User;
 import atos.user.register.repository.UserRepository;
 
-
+//
 @Service
 public class UserService {
    
